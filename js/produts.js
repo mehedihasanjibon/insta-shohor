ad3f0333aec80435bab737a4efa9fb45e7ea3ca7
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-13 22:36:33 +0600

    first commit

---
+++ js/produts.js
@@ -0,0 +1,7 @@
+const products = [
+    {id: 1, name: "mobile" , price: 500, quantity: 5},
+    {id: 2, name: "laptop" , price: 50500, quantity: 15},
+    {id: 3, name: "watch" , price: 2500, quantity: 25},
+    {id: 4, name: "mobile" , price: 500, quantity: 5},
+];
+console.table(products);
